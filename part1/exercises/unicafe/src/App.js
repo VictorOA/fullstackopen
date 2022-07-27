3e083884227e0cae09a5216b36415e92a79477d6
VictorOA <warcvictor@gmail.com>
2022-07-26 21:05:51 -0400

    exercise 1.7 from part 1 completed

--- part1/exercises/unicafe/src/App.js
+++ part1/exercises/unicafe/src/App.js
@@ -14,18 +14,34 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+  const average =(good + (neutral * 0) + (bad * -1))
+  const positive = (good * 100) / all;
+
+  const handleGoodClick = () =>{
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () =>{
+    setNeutral( neutral + 1)
+  }
+  const handleBadlick = () =>{
+    setBad(bad + 1)
+  }
 
 
   return(
 	<div>
     <h1> give feedback</h1>
-		<Button handleClick={() => setGood(good + 1)} text='good' />
-    <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
-    <Button handleClick={() => setBad(bad + 1)} text='bad' />
+		<Button handleClick={handleGoodClick} text='good' />
+    <Button handleClick={handleNeutralClick} text='neutral' />
+    <Button handleClick={handleBadlick} text='bad' />
     <h1> statistics</h1>
     <Display name='good' value={good} />
     <Display name='neutral' value={neutral} />
     <Display name='bad' value={bad} />
+    <Display name='all' value={all} />
+    <Display name='average' value={average/all} />
+    <Display name='positive' value={positive + ' %'} />
 	</div>
   )
 };
